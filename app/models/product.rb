eb630793faecaf44eb6c4323fb9b4c7cc2f2099c
Sam Ruby <649569822@qq.com>
2018-07-31 19:31:36 +0800

    提交

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true,  format: {
+		with:    %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
